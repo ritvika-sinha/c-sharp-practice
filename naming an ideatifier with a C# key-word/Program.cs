d8c0855b8dac7d070dec9fa4cab94fd57128442d
ritvika sinha <42317538+ritvika-sinha@users.noreply.github.com>
2020-04-10 16:14:54 +0530

    naming an ideatifier with a C# key-word

---
+++ naming an ideatifier with a C# key-word/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //naming an ideatifier with a C# key-word
+            string @string = "this is bad practice";
+            Console.WriteLine(@string);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ naming an ideatifier with a C# key-word/readme.txt
@@ -0,0 +1,3 @@
+You can actually name an identifier with a name matching a built-in keyword if you prefix the identifer with the ‘@’ symbol.  
+
+In the following example, we declare a variable of type string and name the variable “string”.  This is legal in C#, but not a good practice.
