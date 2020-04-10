4bd29fcf79c4d6d0ed28a6d4c1b2dda2c3cb458b
ritvika sinha <42317538+ritvika-sinha@users.noreply.github.com>
2020-04-10 15:55:02 +0530

    main should not be public

---
+++ main should not be public/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("this will compile");
+        }
+    }
+}
