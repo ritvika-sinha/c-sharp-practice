5017b7bc6caaf7170b60c8a38d29616de3977070
ritvika sinha <42317538+ritvika-sinha@users.noreply.github.com>
2020-05-12 19:13:38 +0530

    for loop

---
+++ for loop/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loop
+            for (int i = 0, j = 9; i < 10; i++, j--)
+            {
+                Console.WriteLine(i + " " + j);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ for loop/readme.txt
@@ -0,0 +1,3 @@
+Both the initialization clause and the iteration clause of a for loop can consist of multiple statements, separated by commas.
+
+For example, you could initialize two different variables in the initialization clause and then increment one variable and decrement the other in the iteration clause.
