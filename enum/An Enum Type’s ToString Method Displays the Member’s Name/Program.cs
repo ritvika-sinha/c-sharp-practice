67faaa93b4477f18f304def61d6fadc38764cd88
ritvika sinha <42317538+ritvika-sinha@users.noreply.github.com>
2020-05-22 05:19:43 +0530

    enum

---
+++ enum/An Enum Type’s ToString Method Displays the Member’s Name/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{    
+    class Program
+    {
+        public enum Mood
+        {
+            Crabby = -5,
+            Happy = 5,
+            Petulant = -2,
+            Elated = 10
+        };
+
+        static void Main(string[] args)
+        {
+            //An Enum Type’s ToString Method Displays the Member’s Name
+
+            //example 1
+            int x = 42;
+            Console.WriteLine(x.ToString());   // Displays: 42
+            Console.WriteLine(x);     // Same thing--ToString implicitly called
+
+            //example 2
+            Mood myMood = Mood.Crabby;
+            Mood dogsMood = Mood.Elated;
+
+            Console.WriteLine(myMood);     // ToString implicitly called; displays: Crabby
+
+            Console.WriteLine(dogsMood);   // Displays: Elated
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ enum/An Enum Type’s ToString Method Displays the Member’s Name/readme.txt
@@ -0,0 +1,3 @@
+Normally, when you call the ToString method on a variable that stores an integer value, the value of the integer is displayed.(see example 1)
+
+If you call ToString on an enum type variable, however, the textual name of the enumerated member is displayed. (see example 2)
