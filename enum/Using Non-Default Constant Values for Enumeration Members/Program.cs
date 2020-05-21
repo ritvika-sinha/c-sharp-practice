722a9bf794bec1a6215572b7fa1fbb451e43bc0d
ritvika sinha <42317538+ritvika-sinha@users.noreply.github.com>
2020-05-22 05:13:11 +0530

    enum

---
+++ enum/Using Non-Default Constant Values for Enumeration Members/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice
+{    
+    class Program
+    {
+        public enum Mood
+        {
+            Crabby,       // 0
+            Happy,        // 1
+            Petulant,     // 2
+            Elated        // 3
+        };
+        public enum Mood2
+        {
+            Crabby = -5,
+            Happy = 5,
+            Petulant = -2,
+            Elated = 10
+        };
+
+        public enum Weekday
+        {
+            Sunday = 1,
+            Monday,     // 2
+            Tuesday,    // 3, etc.
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+
+        static void Main(string[] args)
+        {
+            //Using Non-Default Constant Values for Enumeration Members
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ enum/Using Non-Default Constant Values for Enumeration Members/readme.txt
@@ -0,0 +1,3 @@
+When you define an enum type, the members contained in your enumeration take on constants starting at zero (for the first member) and then incrementing by one (for consecutive members).
+
+You can, however, specify the constant value to use for each member.  You can specify constants in any order.  Members that don’t have a value specified take on a value one greater than the previous member.
